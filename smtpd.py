da6ce0ca8158f0d03848c05443ef47e73fc83582
Artur Kurkov <kurkov@devexperts.com>
2020-10-06 15:38:24 +0300

    inital project

---
+++ smtpd.py
@@ -0,0 +1,97 @@
+import mailparser
+import sqlite3
+from aiosmtpd.controller import Controller
+import os
+from datetime import datetime
+from web import start_web
+
+
+class DataHandler(object):
+
+    def __init__(self, mailbox_dir, db_name, table_name):
+        self.maildir =os.path.expanduser(mailbox_dir)
+        self.db_name = db_name
+        self.table_name = table_name
+        self.db = f"{self.maildir}/{self.db_name}"
+        self.init_db()
+
+    async def save_mail(self, _from, to, email_title, tm, raw_mail, has_attach):
+        conn = sqlite3.connect(self.db)
+        cur = conn.cursor()
+        sql = f"""
+            insert into {self.table_name} (`email_from`, `email_to`, `email_title`,  `dt`, `email_raw`, `has_attach`) values
+            (?, ?, ?, ?,?, ?)
+        """
+        #print(sql)
+        cur.execute(sql, (_from, to, email_title, tm, raw_mail, has_attach))
+        conn.commit()
+        cur.close()
+        conn.close()
+
+    async def parse(self, from_,to_, raw_mail):
+        mail = mailparser.parse_from_string(raw_mail)
+        email_subject = mail.subject
+        has_attach = 1 if len(mail.attachments)>0 else 0
+        tm = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        print(f"{tm}\t{from_}\t{to_}\t{email_subject}")
+        for ito_ in to_:
+            await self.save_mail(from_, ito_, email_subject,  tm, raw_mail, has_attach)
+
+
+
+    async def handle_DATA(self, server, session, envelope):
+         from_ = envelope.mail_from
+         to_ = envelope.rcpt_tos
+
+         s = envelope.content.decode('utf8', errors='replace')
+         await self.parse(from_, to_, s)
+         return '250 Message accepted for delivery'
+
+    async def handle_RCPT(self, server, session, envelope, address, rcpt_options):
+         #if not address.endswith('@example1.com'):
+         #    return '550 not relaying to that domain'
+         '''
+         add checks email
+         '''
+         envelope.rcpt_tos.append(address)
+         return '250 OK'
+
+    # async def handle_DATA(self, server, session, envelope):
+    #     print('Message from %s' % envelope.mail_from)
+    #     print('Message for %s' % envelope.rcpt_tos)
+    #     print('Message data:\n')
+    #     print(envelope.content.decode('utf8', errors='replace'))
+    #     print('End of message')
+    #     return '250 Message accepted for delivery'
+
+    def init_db(self):
+        print(f"use db {self.db}")
+        conn = sqlite3.connect(self.db)
+        cur = conn.cursor()
+        sql = f"""
+            create table  if not exists  {self.table_name}(
+            id INTEGER  PRIMARY KEY autoincrement,
+            email_from   VARCHAR(255),
+            email_to  VARCHAR(255),
+            email_title VARCHAR(512),
+            dt TEXT,
+            email_raw TEXT,
+            has_attach INTEGER not null
+            )
+        """
+        cur.execute(sql)
+        conn.commit()
+        cur.close()
+        conn.close()
+
+
+if __name__=='__main__':
+
+    try:
+        controller = Controller(DataHandler("~/mailbox", 'fake_mail.db', "fake_mail"), hostname='0.0.0.0', port=25)
+        controller.start()
+
+        start_web("0.0.0.0", "9080")
+    except KeyboardInterrupt:
+        print("smtpd quit!")
---
+++ web.py
@@ -0,0 +1,172 @@
+import sqlite3,os
+from flask import abort
+from flask import make_response
+from flask import Flask, jsonify, redirect, url_for, request
+from flask import render_template
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+DB = os.path.expanduser("~/mailbox/fake_mail.db")
+TABLE = "fake_mail"
+
+def deletemail(id):
+    if id.isnumeric():
+        conn = sqlite3.connect(DB)
+        cur = conn.cursor()
+        sql = f"delete from {TABLE} where id = %s" % request.form["deletemail"]
+        cur.execute(sql)
+        cur.close()
+        conn.commit()
+        conn.close()
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST' and 'deletemail' in request.form:
+       deletemail(request.form["deletemail"])
+
+    curpage=1
+    offset=0
+    limit=10
+
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+    cur.execute(f"select count(*) from {TABLE}")
+    rowcount=int(cur.fetchone()[0])
+    totalpage=(rowcount // limit  )
+    if totalpage*limit < rowcount:
+        totalpage = (rowcount // limit + 1 )
+
+    if request.method:
+        if request.args.get('page'):
+            if request.args.get('page').isnumeric():
+                curpage=int(request.args.get('page'))
+            offset = limit * curpage - limit
+
+    if curpage > totalpage:
+        curpage = totalpage
+        offset = limit * curpage - limit
+
+    startpage = curpage - limit//2
+    endpage = curpage + limit//2
+
+    if startpage < 1:
+        startpage = 1
+        endpage=limit+1
+    elif startpage > 1 and totalpage+1-curpage <= limit//2:
+        endpage=totalpage+1
+        startpage=totalpage+1 - limit
+
+    if limit>=totalpage:
+        startpage = 1
+        endpage = totalpage +1
+
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+    sql = f"""
+            select id, email_title, email_from, email_to, dt, has_attach from {TABLE}  order by id desc  LIMIT {limit} OFFSET {offset}
+        """
+    cur.execute(sql)
+    val = cur.fetchall()
+    cur.close()
+    conn.close()
+
+
+    return render_template("index.html", mails=val, curpage=curpage, totalpage=totalpage, startpage=startpage, endpage=endpage)
+
+@app.route('/search', methods=['GET','POST'])
+def search():
+
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+
+    if request.method=='POST' and 'search' in request.form:
+        if 'deletemail' in request.form:
+            deletemail(request.form["deletemail"])
+        search="%"+request.form['search']+"%"
+        sql = f"select id, email_title, email_from, email_to, dt, has_attach from {TABLE}  WHERE email_title LIKE '%s' order by id desc limit 500" % search
+        cur.execute(sql)
+        val = cur.fetchall()
+        cur.close()
+        conn.close()
+        return render_template("index.html", mails=val)
+    else:
+        return redirect("/")
+
+
+@app.route('/delete_all', methods=['GET'])
+def delete_all():
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+    sql = f"""
+                    delete from {TABLE} 
+                """
+    cur.execute(sql)
+    cur.close()
+    conn.commit()
+    conn.close()
+    return redirect("/")
+
+@app.route('/delete/<int:id>', methods=['GET'])
+def delete(id):
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+    sql = f"delete from {TABLE} where id = %s" % id
+    cur.execute(sql)
+    cur.close()
+    conn.commit()
+    conn.close()
+    return redirect("/")
+
+
+@app.route('/email/<path:email>', methods=['GET'])
+def email(email):
+    """
+    get mail by to email
+    :param email:
+    :param type:
+    :return:
+    """
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+    sql = f"select id,dt,email_raw,email_from, email_to, email_title from {TABLE}  where email_to=? order by id desc limit 1;"
+    cur.execute(sql, [email])
+    val = cur.fetchone()
+    cur.close()
+    conn.close()
+    raw_email = val[2]
+    result = {}
+    result['id'] = val[0]
+    result['from'] = val[3]
+    result['to_'] = val[4]
+    result['title'] = val[5]
+    result['dt'] = val[1]
+    result['raw'] = raw_email
+    return jsonify(result)
+
+
+@app.route('/raw_mail/<int:email_id>', methods=['GET'])
+def raw_mail(email_id):
+    try:
+        conn = sqlite3.connect(DB)
+        cur = conn.cursor()
+        sql = f"select email_raw from {TABLE}  where id=%s" % email_id
+
+        cur.execute(sql)
+        val = cur.fetchone()
+        cur.close()
+        conn.close()
+        raw_email = val[0]
+        response = make_response(raw_email)
+        response.headers.set('Content-Type', 'message/rfc822')
+        response.headers.set('Content-Disposition', 'attachment', filename='%s.eml' % email_id)
+        return response
+    except Exception as err:
+        print(err)
+        abort(404)
+
+def start_web(host, port):
+    app.run(host, port)
+
+if __name__=="__main__":
+    app.run("0.0.0.0", "9080", debug=True)
